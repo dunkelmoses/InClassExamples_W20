e7f614270c4144091ad1f7fe35a4911a9c4ad4aa
Eric Torunski <etorunski@gmail.com>
2020-03-12 16:09:23 -0400

    Finished NavigationDrawer example

--- app/src/main/java/com/example/inclassexamples_w20/MenuExample.java
+++ app/src/main/java/com/example/inclassexamples_w20/MenuExample.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -110,6 +111,9 @@ public class MenuExample extends AppCompatActivity implements NavigationView.OnN
                 message = "You clicked on mail";
                 break;
         }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
 
         Toast.makeText(this, "NavigationDrawer: " + message, Toast.LENGTH_LONG).show();
         return false;
